44f6419a4b6063611a0d674603cf69668d9ac8b9
ethankclam <ethankclam@gmail.com>
2018-08-22 18:04:25 +0800

    variable flating point number

--- js/variable.js
+++ js/variable.js
@@ -1,5 +1,8 @@
-var x = 2018
-document.write(x)
+var x = 2018;
+document.write(x);
 
-var y = "two thousand and eight"
-document.write(y)
+var y = "two thousand and eight";
+document.write(y);
+
+var price = 55.55;
+document.write(price);
