9241616828dc577264a0c26450b6f1d60865cf61
ethankclam <ethankclam@gmail.com>
2019-05-20 12:12:52 +0800

    new objects

---
+++ Objects/Object.js
@@ -0,0 +1,5 @@
+let dog {
+	name: "Donkey",
+	numLegs: 4,
+	color: "White",
+};
---
+++ Objects/accessjsobject.js
@@ -0,0 +1,7 @@
+let dog {
+	name: "Donkey",
+	numLegs: 4,
+	color: "White",
+};
+
+console.log(dog.name, dog.numLegs, dog.color);
